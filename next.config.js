681ee7ecd78b7599eb83b60c1b1687b6c5d41885
甜甜 <nbios@qq.com>
2026-09-09 14:10:14 +0800

    feat: 适配自定义域名，移除basePath和assetPrefix子路径配置

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repo = 'my-medbox';
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
   trailingSlash: true,
 };
 
