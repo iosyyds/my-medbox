1549221be7e50442abf2c1c05f2c0cd6c09644bf
甜甜 <nbios@qq.com>
2026-09-09 13:41:51 +0800

    fix: 锁定手机端缩放，添加user-scalable=no和viewport-fit=cover

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,9 @@ export const viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
+  minimumScale: 1,
+  userScalable: 'no',
+  viewportFit: 'cover',
   themeColor: '#0d9488',
 };
 
